f4a84e03eaa7e2db1764de298f3e1347d131a1d9
loli <treli.lala@hotmail.fr>
2016-06-08 23:49:39 +0200

    Premier commit Portfolio

---
+++ WebSite/JS/MenuController.js
@@ -0,0 +1,43 @@
+﻿function MenuController($scope) {
+    var items = [
+        {
+            Name: "Home",
+            Href: "#/home",
+            Active: true
+        },
+        {
+            Name: "Expérience",
+            Href: "#/experience",
+            Active: false
+        },
+        {
+            Name: "Compétences",
+            Href: "#/competence",
+            Active: false
+        },
+        {
+            Name: "Loisirs",
+            Href: "#/loisir",
+            Active: false
+        },
+        {
+            Name: "Contact",
+            Href: "#/contact",
+            Active: false
+        }
+    ];
+    var Menu = {};
+    Menu.Items = items;
+    var index = 0;
+    Menu.Change = function (index) {
+        for (var i = 0; i < Menu.Items.length; i++) {
+            if(i == index){
+                Menu.Items[i].Active = true;
+            } else {
+                Menu.Items[i].Active = false;
+            }
+        }
+    }
+
+    $scope.Menu = Menu;
+}
---
+++ WebSite/JS/Route.js
@@ -0,0 +1,37 @@
+﻿function Route($stateProvider, $urlRouterProvider) {
+    //Set default route
+    $urlRouterProvider.otherwise('/');
+ 
+    //Declare states
+    $stateProvider
+      .state('home', {
+          url: '/',
+          templateUrl: 'HTML/home.html',
+          controller: 'HomeController',
+          controllerAs: 'Home'
+      })
+      .state('Menu1', {
+          url: '/experience',
+          templateUrl: 'HTML/Experience.html',
+          controller: 'ExperienceController',
+          controllerAs: 'Experience'
+      })
+      .state('Competence', {
+          url: '/competence',
+          templateUrl: 'HTML/Competence.html',
+          controller: 'CompetenceController',
+          controllerAs: 'Competence'
+      })
+      .state('loisir', {
+          url: '/loisir',
+          templateUrl: 'HTML/Loisir.html',
+          controller: 'LoisirController',
+          controllerAs: 'Loisir'
+      })
+      .state('contact', {
+          url: '/contact',
+          templateUrl: 'HTML/Contact.html',
+          controller: 'ContactController',
+          controllerAs: 'Contact'
+      });
+}
