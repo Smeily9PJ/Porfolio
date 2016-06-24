364cfd868cb083d8b85a489e1283ef7a12f35b0a
Smeily9PJ <siclebree.fairway@hotmail.fr>
2016-06-24 10:38:09 +0200

    LE BROWSER SYNC FONCTIONNE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- WebSite/Gulpfile.js
+++ WebSite/Gulpfile.js
@@ -10,18 +10,21 @@ gulp.task('sass', function () {
     gulp.src('App/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('App/Generate/Css'))
-        .pipe(browserSync.stream());
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('browserSync', function () {
-    browserSync.init(null, {
-        server: {
-            baseDir: './'
-        }
+    browserSync.init({
+        proxy: 'localhost:59803'
     });
-});
+})
 
 gulp.task('watch', ['browserSync', 'sass'], function () {
-    gulp.watch('App/**/*.scss', ['sass']).on('change', browserSync.reload);
+    gulp.watch('App/**/*.scss', ['sass']);
     // Other watchers
+    gulp.watch('App/**/*.html', browserSync.reload);
+    gulp.watch('App/**/*.js', browserSync.reload);
+    gulp.watch('index.html', browserSync.reload);
+    gulp.watch('App.js', browserSync.reload);
+    gulp.watch('Route.js', browserSync.reload);
 });
